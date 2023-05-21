fbbff3bbb0a2b17bb3c11edb8f37428693a9f7bd
casey <caseyochieng46@gmail.com>
2023-05-21 22:54:33 +0300

    made a responsive navbar

--- js/toggle.js
+++ js/toggle.js
@@ -4,4 +4,6 @@ const dropDown_menu = document.querySelector(".dropdown_menu");
 
 toggleBtnIcon.onclick = () => {
     dropDown_menu.classList.toggle("open");
+    const isOpen = dropDown_menu.classList.contains("open");
+    toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 }
