b5f4f84ac8935a4995425845e99f5f075f858796
casey <caseyochieng46@gmail.com>
2023-05-25 01:23:32 +0300

    can send email

--- js/email.js
+++ js/email.js
@@ -2,7 +2,7 @@ function sendEmail(){
     Email.send({
         Host : "smtp.gmail.com",
         Username : "twoy46@gmail.com",
-        Password : "ochiengcasey28",
+        Password : "password",
         To : 'twoy46@gmail.com',
         From : document.getElementById("email").value,
         Subject : "New Contact Form Enquiry",
