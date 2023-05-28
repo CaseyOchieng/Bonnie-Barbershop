aee25f7e127f0cdf0dba6b61f2157829292f1b65
casey <caseyochieng46@gmail.com>
2023-05-28 15:20:08 +0300

    added send email

--- js/email.js
+++ js/email.js
@@ -5,10 +5,10 @@
 
 function sendEmail(){
     Email.send({
-        Host : "smtp.gmail.com",
-        Username : "twoy46@gmail.com",
-        Password : "password",
-        To : 'twoy46@gmail.com',
+        Host : "smtp.elasticemail.com",
+        Username : "shopbonnie001@gmail.com",
+        Password : "FDC64C5293269F0DA3797001F2E34D9FF223",
+        To : 'shopbonnie001@gmail.com',
         From : document.getElementById("email").value,
         Subject : "New Contact Form Enquiry",
         Body : "Name: "+document.getElementById("name").value
